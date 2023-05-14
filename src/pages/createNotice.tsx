88700ff8817618a7c11e7ff3ad04ff263e15dbdb
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2023-05-14 13:00:47 -0300

    Create createNotices.tsx

---
+++ src/pages/createNotice.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { parseCookies } from 'nookies';
+import { createNotice } from '../utils/api';
+import Header from '../components/Header';
+import NoticeForm from '../components/NoticeForm';
+
+export default function CreateNotice() {
+  const router = useRouter();
+  const [errorMessage, setErrorMessage] = useState('');
+
+  async function handleCreateNotice(data) {
+    try {
+      const token = parseCookies().token;
+      await createNotice(data, token);
+      router.push('/notices');
+    } catch (err) {
+      console.error(err);
+      setErrorMessage('Erro ao criar recado');
+    }
+  }
+
+  function handleCancel() {
+    router.push('/notices');
+  }
+
+  return (
+    <>
+      <Header />
+      <div>
+        <h1>Criar novo recado</h1>
+        <NoticeForm onSubmit={handleCreateNotice} onCancel={handleCancel} />
+        {errorMessage && <p>{errorMessage}</p>}
+      </div>
+    </>
+  );
+}
